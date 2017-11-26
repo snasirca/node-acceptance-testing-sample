62397b12ae5721f973b7f3296c4e07e18da5f6b3
Shahriyar Nasir <contact@snasir.ca>
2017-11-25 19:36:04 -0500

    Removes unnecessary code

--- spec/acceptance/browse.spec.js
+++ spec/acceptance/browse.spec.js
@@ -15,7 +15,6 @@ describe('webdriver.io api page', function () {
     const result = await browser.getTitle()
 
     expect(result).toEqual('Home | nayema.github.io')
-    // expect(result).toEqual('WebdriverIO - WebDriver bindings for Node.js1111111111')
   })
 
   it('123should be able to filter for commands', async function () {
