835ebe9a6b718a0321c03be856f199b38efd1f1e
Shahriyar Nasir <contact@snasir.ca>
2017-11-27 11:05:02 -0500

    Adds nodemon to restart a dev server automatically

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', (req, res, next) => {
+  const a = 'jkssssl'
+  res.render('index', { title: a })
+})
 
-module.exports = router;
+module.exports = router
